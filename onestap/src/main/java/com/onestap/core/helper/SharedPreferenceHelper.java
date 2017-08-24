8ddefca2e9ab9f66cca4ba80e0800cbe0b2b7135
Marcos Gribel <gribel.marcos@gmail.com>
2017-08-24 15:12:52 -0300

    remove third party libraries to save data in shared preferences

---
+++ onestap/src/main/java/com/onestap/core/helper/SharedPreferenceHelper.java
@@ -0,0 +1,11 @@
+package com.onestap.core.helper;
+
+/*
+* Copyright (c) 2017 - All Rights Reserved
+* Unauthorized copying of this file, via any medium is strictly prohibited.
+* Proprietary and confidential.
+*
+* @author Marcos Gribel - gribel.marcos@gmail.com
+*/
+public class SharedPreferenceHelper {
+}
