80bbf838f73329aec3a05d983fc8f100487444aa
mgribel <gribel.marcos@gmail.com>
2017-08-30 12:16:12 -0300

    create boundary to accest sdk methods for user

--- onestap/src/main/java/com/onestap/user/OSTUser.java
+++ onestap/src/main/java/com/onestap/user/OSTUser.java
@@ -1,4 +1,20 @@
+/*
+ * Copyright (c) Stone - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ */
+
 package com.onestap.user;
+
+import android.content.Context;
+
+import com.onestap.core.model.domain.boundary.CallbackBoundary;
+import com.onestap.user.model.domain.entities.AccountResponse;
+import com.onestap.user.model.domain.entities.PendingProfile;
+import com.onestap.user.model.domain.entities.TempProfile;
+import com.onestap.user.model.usecase.UserUseCase;
+import com.onestap.user.presenter.contract.UserContract;
 
 /**
  * Created on 30/08/2017
@@ -7,5 +23,25 @@ package com.onestap.user;
  * @email mrebelo@stone.com.br
  */
 
-public class OSTUser {
+public final class OSTUser implements UserContract {
+
+
+    private Context context;
+    private UserContract.UseCase useCase;
+
+    public OSTUser(Context context) {
+        this.context = context;
+        this.useCase = new UserUseCase(context);
+    }
+
+
+    @Override
+    public void savePendingProfile(TempProfile body, CallbackBoundary<PendingProfile> callbackBoundary) {
+        useCase.savePendingProfile(body, callbackBoundary);
+    }
+
+    @Override
+    public void getUser(CallbackBoundary<AccountResponse> callbackBoundary, String... categories) {
+        useCase.getUser(callbackBoundary, categories);
+    }
 }
