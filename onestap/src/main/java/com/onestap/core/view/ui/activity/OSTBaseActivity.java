8bee0f5e51de658626c722413dd176ceb35ca719
mgribel <gribel.marcos@gmail.com>
2017-08-30 16:36:16 -0300

    fix issue #5

--- onestap/src/main/java/com/onestap/core/view/ui/activity/OSTBaseActivity.java
+++ onestap/src/main/java/com/onestap/core/view/ui/activity/OSTBaseActivity.java
@@ -26,37 +26,10 @@ import static android.os.Build.VERSION_CODES.LOLLIPOP;
 
 public abstract class OSTBaseActivity extends Activity {
 
-    WebView webView;
-    ProgressBar connectProgressBar;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        connectProgressBar = findViewById(R.id.progress);
-    }
 
     protected abstract String urlToLoad();
 
     protected abstract WebViewClient client();
 
-    public void loadWebView() {
-        webView = findViewById(R.id.web_view_ost);
-        webView.setWebViewClient(client());
-        webView.loadUrl(urlToLoad());
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.getSettings().setAppCacheEnabled(true);
-        CookieManager.getInstance().setAcceptCookie(true);
-        if (SDK_INT >= LOLLIPOP)
-            CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true);
-    }
-
-
-    public void showProgress() {
-        connectProgressBar.setVisibility(View.VISIBLE);
-    }
-
-    public void hideProgress() {
-        connectProgressBar.setVisibility(View.GONE);
-    }
+    protected abstract void loadWebView();
 }
